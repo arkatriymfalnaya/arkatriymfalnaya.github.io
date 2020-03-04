997a3d8d3ad2b438945d7478f417c3ebf0d59ecf
uknowwhat <rakovskiya@yandex.ru>
2020-03-04 21:32:21 +0300

    added html, fixed scroll for wide screens

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ class App extends Component {
         super(props);
 
         this.state = {
-            goodsQuantity: 2,
+            goodsQuantity: 10,
             goodsList: []
         };
     }
 
 
   handleScroll = () => {
-    if ((window.innerWidth + window.scrollX) >= document.querySelector('.grid').scrollWidth) {
-
-      this.loadGoods(this.state.goodsQuantity++);
+    if ((window.innerWidth + document.querySelector('.cards__grid').scrollLeft) >= document.querySelector('.cards__grid').scrollWidth) {
+        this.loadGoods(this.state.goodsQuantity++);
     }
   }
 
@@ -35,17 +34,46 @@ class App extends Component {
   }
 
   componentDidMount() {
-      window.addEventListener('scroll', this.handleScroll);
+      document.querySelector('.cards__grid').addEventListener('scroll', this.handleScroll);
   }
 
   render() {
 
     return (
-        <div className="grid">
+        <div className="cards__grid">
+
+            <div className="card card_heading">
+                <div className="card__rating">Рейтинг</div>
+                <div className="card__price">Цена</div>
+                <div className="card__color">Цвет</div>
+                <div className="card__material">Материал</div>
+                <div className="card__size">Размер</div>
+                <div className="card__mechanism">Механизм</div>
+                <div className="card__seller">Продавец</div>
+            </div>
             {this.state.goodsList.map((item) => (
-                <li className="item" key={item.id}>{item.id} {item.name}
-                <img src={ item.picture } />
-                </li>
+                <div id={ item.id } className="card">
+                    <div className="card__picture">
+                        <img src={ item.picture } alt="Изображение { item.name.toLowerCase() }"/>
+                    </div>
+                    <div className="card__title">{ item.name }</div>
+                    <div className="card__rating">{ item.rating }</div>
+                    <div className="card__price">
+                        { item.price }
+                        {item.newPrice &&
+                            <span>{ item.newPrice }</span>
+                          }
+                    </div>
+                    <div className="card__color">{ item.color }</div>
+                    <div className="card__material">{ item.material }</div>
+                    <div className="card__size">{ item.size }</div>
+                    <div className="card__mechanism">{ item.mechanism }</div>
+                    <div className="card__seller"><a href="" title={ item.sellerLink } target="_blank">{ item.seller }</a></div>
+                    <div className="card__seller">
+                        <button className="button button_like">like</button>
+                        <button className="button button_buy">buy</button>
+                    </div>
+                </div>
             ))}
         </div>
     );
