591cde9b3a5b7073287ef2420009a71a3b92956e
uknowwhat <rakovskiya@yandex.ru>
2020-03-05 01:39:13 +0300

    added styling, rating component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import Rating from '@material-ui/lab/Rating';
+
 import './App.css';
 
 class App extends Component {
@@ -13,6 +15,7 @@ class App extends Component {
     }
 
 
+
   handleScroll = () => {
     if ((window.innerWidth + document.querySelector('.cards__grid').scrollLeft) >= document.querySelector('.cards__grid').scrollWidth) {
         this.loadGoods(this.state.goodsQuantity++);
@@ -20,7 +23,7 @@ class App extends Component {
   }
 
   loadGoods = (quantity) => {
-      fetch('https://api.jsonbin.io/b/5e5fd9bbbaf60366f0e2cf61/3')
+      fetch('https://api.jsonbin.io/b/5e5fd9bbbaf60366f0e2cf61/7')
         .then((response) => response.json())
         .then(data => {
             let result = data.filter((item, id) => id <= this.state.goodsQuantity + quantity);
@@ -37,45 +40,99 @@ class App extends Component {
       document.querySelector('.cards__grid').addEventListener('scroll', this.handleScroll);
   }
 
+
   render() {
 
     return (
-        <div className="cards__grid">
+        <section className="cards__grid">
 
-            <div className="card card_heading">
-                <div className="card__rating">Рейтинг</div>
-                <div className="card__price">Цена</div>
-                <div className="card__color">Цвет</div>
-                <div className="card__material">Материал</div>
-                <div className="card__size">Размер</div>
-                <div className="card__mechanism">Механизм</div>
-                <div className="card__seller">Продавец</div>
-            </div>
+            <article className="card card_heading">
+                <ul className="card__characteristics">
+                    <li className="card__rating">
+                        <strong>Рейтинг</strong>
+                    </li>
+                    <li className="card__price">
+                        <strong>Цена</strong>
+                    </li>
+                    <li className="card__color">
+                        <strong>Цвет</strong>
+                    </li>
+                    <li className="card__material">
+                        <strong>Материал</strong>
+                    </li>
+                    <li className="card__size">
+                        <strong>Размер</strong>
+                    </li>
+                    <li className="card__mechanism">
+                        <strong>Механизм</strong>
+                    </li>
+                    <li className="card__seller">
+                        <strong>Продавец</strong>
+                    </li>
+                </ul>
+            </article>
+            
             {this.state.goodsList.map((item) => (
-                <div id={ item.id } className="card">
-                    <div className="card__picture">
-                        <img src={ item.picture } alt="Изображение { item.name.toLowerCase() }"/>
-                    </div>
-                    <div className="card__title">{ item.name }</div>
-                    <div className="card__rating">{ item.rating }</div>
-                    <div className="card__price">
-                        { item.price }
-                        {item.newPrice &&
-                            <span>{ item.newPrice }</span>
-                          }
-                    </div>
-                    <div className="card__color">{ item.color }</div>
-                    <div className="card__material">{ item.material }</div>
-                    <div className="card__size">{ item.size }</div>
-                    <div className="card__mechanism">{ item.mechanism }</div>
-                    <div className="card__seller"><a href="" title={ item.sellerLink } target="_blank">{ item.seller }</a></div>
-                    <div className="card__seller">
-                        <button className="button button_like">like</button>
-                        <button className="button button_buy">buy</button>
-                    </div>
-                </div>
+                <article key={ item.id } className="card" tabIndex="1">
+                    <figure className="card__picture">
+                            <img src={ item.picture } alt={ `Изображение ${item.name.toLowerCase()}` }/>
+                    </figure>
+                    <a className="card__title" href="#" title={ `Смотреть товар ${ item.name }` }>
+                        <strong>{ item.name }</strong>
+                    </a>
+                    <ul className="card__characteristics">
+                        <li className="card__rating">
+                            <Rating
+                              name="read-only"
+                              value={ +item.rate }
+                              readOnly
+                            />
+                            <strong>{ item.rate }</strong>
+                        </li>
+                        { item.newPrice ? (
+                            <li className="card__price">
+                                <strong className="card__price_new">{ item.newPrice }</strong>
+                                <span className="card__price_old">{ item.price }</span>
+                            </li>
+                        ) : (
+                            <li className="card__price">
+                                <span className="card__price">{ item.price }</span>
+                            </li>
+                        )}
+                        <li className="card__color">
+                            { item.color }
+                        </li>
+                        <li className="card__material">
+                            { item.material }
+                        </li>
+                        <li className="card__size">
+                            { item.size }
+                        </li>
+                        <li className="card__mechanism">
+                            { item.mechanism }
+                        </li>
+                        <li className="card__seller">
+                            <a
+                                href={ item.sellerLink }
+                                title={ `Перейти на сайт ${ item.seller }` }
+                                target="_blank">
+                                    { item.seller }
+                            </a>
+                        </li>
+                    </ul>
+                    { item.likesBlocked ? (
+                        <section className="card__functionality">
+                            <button className="button button_buy" data-expanded="true">Купить</button>
+                        </section>
+                    ) : (
+                        <section className="card__functionality">
+                            <button className="button button_like"></button>
+                            <button className="button button_buy">Купить</button>
+                        </section>
+                    )}
+                </article>
             ))}
-        </div>
+        </section>
     );
   }
 }
