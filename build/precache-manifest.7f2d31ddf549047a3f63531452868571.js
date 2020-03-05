659763f200b8c5728f4cdea8c563078a97d4e42b
uknowwhat <rakovskiya@yandex.ru>
2020-03-05 12:46:21 +0300

    disassembled into functional components, cleared console warnings, rewrited readme

---
+++ build/precache-manifest.7f2d31ddf549047a3f63531452868571.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf89e3218a69614a87329f18fa0b290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50ab89681c79f81e1656",
+    "url": "/static/css/main.1512c18b.chunk.css"
+  },
+  {
+    "revision": "04c54799b3109cd3a032",
+    "url": "/static/js/2.7a81cf9e.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.7a81cf9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ab89681c79f81e1656",
+    "url": "/static/js/main.df5de624.chunk.js"
+  },
+  {
+    "revision": "23e4442565e6e469f709",
+    "url": "/static/js/runtime-main.17f2c86e.js"
+  },
+  {
+    "revision": "7770e1f9a027ff0867dfb24537eb49e2",
+    "url": "/static/media/icon_cart.7770e1f9.svg"
+  },
+  {
+    "revision": "6c6a814ee14e2d918c835a39ac8bef81",
+    "url": "/static/media/icon_cart_hover.6c6a814e.svg"
+  },
+  {
+    "revision": "8b0126c677112ff8ac1c581569efaae2",
+    "url": "/static/media/icon_heart.8b0126c6.svg"
+  },
+  {
+    "revision": "876106c66a034b34abd8d0c8cdccbdcd",
+    "url": "/static/media/icon_heart_hover.876106c6.svg"
+  }
+]);
