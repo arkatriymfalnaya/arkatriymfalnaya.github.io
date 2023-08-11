04932ad8344d2c20c5798b79ea3a29821164a81f
dog <arkadyrakovsky@gmail.com>
2023-08-11 10:55:05 +0300

    initialize astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://arkatriymfalnaya.github.io/",
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "nord",
+    },
+    remarkPlugins: ["remark-gfm", "remark-smartypants"],
+    rehypePlugins: [
+      [
+        "rehype-external-links",
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+  },
 });
