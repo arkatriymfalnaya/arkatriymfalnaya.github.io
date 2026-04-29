dbec8cfcebc52595cea41cb9cfd9cb84ec6359a7
dog <arkadyrakovsky@gmail.com>
2026-04-29 17:56:33 +0300

    minor fixes

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "arka triymfalnaya",
 	url: "https://arkatriymfalnaya.github.io/",
 	language: "ru",
-	description: "another shitty blog",
+	description: "мой пёс стал человеком, а я еще в пути",
 	keywords:
 		"it computer science web development frontend reactjs solidjs js ts html css nextjs nodejs rust browsers переводы статьи фронтенд веб разработка",
 	author: {
