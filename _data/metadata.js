39ce9816cbe6eac16f7c43f52581130824c4f75d
dog <arkadyrakovsky@gmail.com>
2026-04-29 15:20:28 +0300

    add keywords & projects

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,9 @@ export default {
 	title: "arka triymfalnaya",
 	url: "https://arkatriymfalnaya.github.io/",
 	language: "ru",
-	description: "",
+	description: "another shitty blog",
+	keywords:
+		"it computer science web development frontend reactjs solidjs js ts html css nextjs nodejs rust browsers переводы статьи фронтенд веб разработка",
 	author: {
 		name: "Arkadzi Rakouski",
 		email: "arkadyrakovsky@gmail.com",
